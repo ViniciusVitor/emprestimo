9b50a885d9af6a402c3e750ec199bffe3bf571fc
Vinicius Vitor <vinnicius6060@gmail.com>
2025-08-26 14:04:35 -0300

    ajuste telemetria

--- src/main/java/com/simulador/emprestimo/interceptor/MetricsInterceptor.java
+++ src/main/java/com/simulador/emprestimo/interceptor/MetricsInterceptor.java
@@ -8,23 +8,47 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import reactor.netty.http.server.HttpServerRequest;
 import reactor.netty.http.server.HttpServerResponse;
 
+import java.util.List;
+
 @Component
 public class MetricsInterceptor implements HandlerInterceptor {
 
+
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse reponse, Object handler){
-        request.setAttribute("startTime", System.currentTimeMillis());
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        String uri = request.getRequestURI();
+
+        // Lista de URIs que devem ser ignoradas
+        List<String> urisIgnoradas = List.of(
+                "/swagger-ui", "/swagger-ui/swagger-ui.css", "/v3/api-docs/swagger-config",
+                "/v3/api-docs", "/swagger-ui/swagger-initializer.js", "/swagger-ui/index.css",
+                "/swagger-ui/swagger-ui-standalone-preset.js",  "/swagger-ui/index.html",
+                "/swagger-ui/swagger-ui-bundle.js", "/swagger-resources", "/h2-console"
+        );
+
+        boolean ignorar = urisIgnoradas.stream().anyMatch(uri::startsWith);
+
+        if (!ignorar) {
+            request.setAttribute("startTime", System.currentTimeMillis());
+        }
+
         return true;
     }
 
     @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex){
-        long startTime = (long) request.getAttribute("startTime");
-        long duration = System.currentTimeMillis() - startTime;
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        Object startTimeAttr = request.getAttribute("startTime");
 
-        String endpoint = request.getRequestURI();
-        boolean sucesso = response.getStatus() == 200;
+        if (startTimeAttr instanceof Long) {
+            long startTime = (long) startTimeAttr;
+            long duration = System.currentTimeMillis() - startTime;
 
-        MetricasRepository.getOrCreate(endpoint).registrarRequisicao(duration, sucesso);
+            String endpoint = request.getRequestURI();
+            boolean sucesso = response.getStatus() == 200;
+
+            MetricasRepository.getOrCreate(endpoint).registrarRequisicao(duration, sucesso);
+        }
+
     }
+
 }
